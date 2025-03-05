fd1503166db36f01c86d440aa456391a0a604854
RubenD1997 <ruben28-1997@hotmail.com>
2025-03-05 11:56:21 -0500

    Changes Dagger Hilt

--- app/src/main/java/com/example/logincompose/core/network/RetrofitHelper.kt
+++ app/src/main/java/com/example/logincompose/core/network/RetrofitHelper.kt
@@ -4,6 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
+
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://run.mocky.io/")
